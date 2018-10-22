77093ead728dbaae29a9ac581ac6ab140ac9b76a
Hallvard Traetteberg <hal@ntnu.no>
2018-10-22 22:00:30 +0200

    Slightly improved.

--- tdt4100.exam.v2018/src/farkle/Dice.java
+++ tdt4100.exam.v2018/src/farkle/Dice.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
  */
 public class Dice implements Iterable<Integer> {
 
-	/**
+	/** (part 1 a)
 	 * @param dieCount
 	 * @return a collection of random integer values in the range 1-6
 	 */
@@ -20,7 +20,7 @@ public class Dice implements Iterable<Integer> {
 
 	// fields (part 1) ???
 
-	/** (part 1)
+	/** (part 1 a)
 	 * Initializes this Dice with the values in dieValues, and a score.
 	 * @param dieValues
 	 * @param score the score to set, may be -1 for yet unknown
@@ -30,7 +30,7 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 1)
+	/** (part 1 a)
 	 * Initializes this Dice with dieCount random values (using Math.random())
 	 * @param dieCount
 	 */
@@ -38,7 +38,7 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 1)
+	/** (part 1 a)
 	 * Initializes this Dice with the values in dice, and a score
 	 * @param dieValues // Denne skulle vært bare "dice", ikke "dieValues"
 	 * @param score the score to set, may be -1 for yet unknown
@@ -47,19 +47,21 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 18)
+	/** (part 5)
 	 * Initializes this Dice with n die values provided by the supplier argument.
 	 * @param dieCount the number of dice to "throw"
 	 * @param supplier provides the die values
 	 */
-	public Dice(final int dieCount, final Supplier<Integer> supplier) { ... }
+	public Dice(final int dieCount, final Supplier<Integer> supplier) {
+		// ... ???
+	}
 
-	/** (part 2)
+	/** (part 1 b)
 	 * Format: [die1, die2, ...] = score (score is omitted when < 0)
 	 */
 	/// method for generating a String representation of a Dice instance ???
 
-	/** (part 2)
+	/** (part 1 b)
 	 * Parses a string using the toString format (see above) and
 	 * returns a corresponding Dice.
 	 * @param s
@@ -69,14 +71,14 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 3)
+	/** (part 1 c)
 	 * @return the number of die values
 	 */
 	public int getDieCount() {
 		// ???
 	}
 
-	/** (part 3)
+	/** (part 1 c)
 	 * @param dieNum
 	 * @return the value of die number dieNum
 	 */
@@ -84,7 +86,7 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 3)
+	/** (part 1 c)
 	 * @param value
 	 * @return the number of dice with the provided value
 	 */
@@ -92,14 +94,14 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 4)
+	/** (part 1 d)
 	 * @return the current score
 	 */
 	public int getScore() {
 		// ???
 	}
 
-	/** (part 4)
+	/** (part 1 d)
 	 * Sets the score, but only if it isn't already set to a non-negative value
 	 * @param score
 	 * @throws a suitable exception if score already is set to a non-negative value
@@ -108,17 +110,17 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	// ... Iterable methods (part 5) ???
+	// ... Iterable methods (part 1 e) ???
 
-	/** (part 6) // Denne ble det ikke spurt om, og det var ikke meningen at den skulle implementeres, men den kunne brukes
+	/**
 	 * @param dice
 	 * @return true if all die values in the argument appear in this Dice
 	 */
 	public boolean contains(final Dice dice) {
-		// ???
+		// You don't need to implement it, but you can use it
 	}
 
-	/** (part 6)
+	/** (part 1 f)
 	 * @param dices a Collection of Dice // Denne linja var feil, det skulle være bare "dice a Dice"
 	 * @return a new Dice instance with the all the die values this Dice and
 	 * all Dice in the argument, without any specific order
@@ -127,7 +129,7 @@ public class Dice implements Iterable<Integer> {
 		// ???
 	}
 
-	/** (part 6)
+	/** (part 1 f)
 	 * @param dice
 	 * @return a new Dice instance with the die values from this Dice, but
 	 * without those from the argument, without any specific order
