564c8f0fbf80f57cbd648a338015ba7320ee74f2
Jørgen Stamnes <jorghs@stud.ntnu.no>
2019-08-27 10:14:44 +0000

    Testing

--- py/test.py
+++ py/test.py
@@ -1,1 +1,13 @@
 # Write a program til inputs n integers and find the mean and median
+
+def getNumberOfIntegers(numberOfIntegers):
+    return [0,1,2,3,4,5,6]
+
+def findMean(integerList):
+    return 2
+
+def findMedian(integerList):
+    return 3
+
+print(getNumberOfIntegers(4))
+
